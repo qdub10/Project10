3807a38181e8833af090b6682def05d2752343be
quinnw10 <jquinnw10@gmail.com>
2024-10-23 12:09:00 -0400

    final changes

--- Project10/seeds.sql
+++ Project10/seeds.sql
@@ -1,8 +1,10 @@
 INSERT INTO department (name) VALUES ('Engineering'), ('Finance'), ('HR');
+ON CONFLICT (name) DO NOTHING;
 INSERT INTO role (title, salary, department_id) VALUES 
   ('Software Engineer', 80000, 1), 
   ('Accountant', 60000, 2), 
   ('HR Manager', 70000, 3);
+ON CONFLICT (title) DO NOTHING;
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES 
   ('John', 'Doe', 1, NULL),
   ('Jane', 'Smith', 2, NULL),
